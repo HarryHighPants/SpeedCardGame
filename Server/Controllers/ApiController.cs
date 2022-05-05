31227c649c17f1d92632a3e123405f6a33ceaaf9
HarryHighPants <38173749+HarryHighPants@users.noreply.github.com>
2022-05-05 23:28:30 +1000

    Added ApiController

---
+++ Server/Controllers/ApiController.cs
@@ -0,0 +1,101 @@
+namespace Server.Controllers;
+
+using System.Linq;
+using Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Models.Database;
+using Services;
+
+[ApiController]
+[Route("api")]
+public class ApiController : ControllerBase
+{
+	private readonly GameResultContext gameResultContext;
+
+	public ApiController(GameResultContext gameResultContext)
+	{
+		this.gameResultContext = gameResultContext;
+	}
+
+	[HttpGet]
+	public IActionResult GetGamesPlayed()
+	{
+		return Ok(gameResultContext.GameResults.Count());
+	}
+
+	[HttpGet("top-players")]
+	public IActionResult TopPlayers()
+	{
+		return Ok(gameResultContext.Players.OrderByDescending(p=>p.Elo).Take(10).Select(p=>new PlayerApi(p)).ToList());
+	}
+
+	[HttpGet("player/{playerId}")]
+	public IActionResult TopPlayers(Guid playerId)
+	{
+		var player = gameResultContext.Players.Find(playerId);
+		if (player == null)
+		{
+			return BadRequest("Could not find player with that id");
+		}
+		return Ok(new PlayerApi(player));
+	}
+
+	[HttpGet("ping")]
+	public IActionResult Ping()
+	{
+		return Ok(true);
+	}
+
+	[HttpGet("daily-games-shield-data")]
+	public IActionResult DailyGamesShieldData()
+	{
+		var playersCount = GamesPlayed(1);
+		var shieldData = new ShieldData("Games today", playersCount.ToString());
+		return Ok(shieldData);
+	}
+
+	[HttpGet("weekly-games-shield-data")]
+	public IActionResult WeeklyGamesShieldData()
+	{
+		var playersCount = GamesPlayed(7);
+		var shieldData = new ShieldData("Weekly Games", playersCount.ToString());
+		return Ok(shieldData);
+	}
+
+	[HttpGet("monthly-games-shield-data")]
+	public IActionResult MonthlyGamesShieldData()
+	{
+		var playersCount = GamesPlayed(31);
+		var shieldData = new ShieldData("Monthly Games", playersCount.ToString());
+		return Ok(shieldData);
+	}
+
+	private int GamesPlayed(int? inDays = null)
+	{
+		var dailyIndex = GameHub.GetDayIndex();
+		return gameResultContext.GameResults.Count(g => inDays == null || g.DailyIndex >= dailyIndex-inDays);
+	}
+
+	// https://shields.io/endpoint
+	record ShieldData(string label, string message, string? color = "lightgrey", int? schemaVersion = 1);
+
+	class PlayerApi
+	{
+		public string Name { get; set; }
+		private int Elo { get; set; }
+		private string Rank { get; set; }
+		private int Wins { get; set; }
+		private int Losses { get; set; }
+		private int DailyStreak { get; set; }
+
+		public PlayerApi(PlayerDao playerDoa)
+		{
+			Name = playerDoa.Name;
+			Elo = playerDoa.Elo;
+			Rank = InMemoryGameService.GetRank(playerDoa.Elo).ToString();
+			Wins = playerDoa.DailyWins;
+			Losses = playerDoa.DailyLosses;
+			DailyStreak = playerDoa.DailyWinStreak;
+		}
+	}
+}
